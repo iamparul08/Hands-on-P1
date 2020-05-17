040590f492e76a4f6fe82924f23feec0009532e0
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-17 23:03:27 +0530

    Add files via upload

---
+++ intr2_ADID.py
@@ -0,0 +1,9 @@
+
+name = input("Enter your name: ")
+print("Your name is: " + name)
+
+no = input("Enter your employee number: ")
+print("Your employee no is: " + no)
+
+cname = input("Enter your company name: ")
+print("Your compmany name is: " + cname)
